ebf7271db8168c11536f04f13bb18b42ef86b3da
benjaminVic <benjamin.vic.lallart@gmail.com>
2015-03-27 17:17:39 +0100

    MOAR STATMENT 2
    Because fuck you cocoun548

--- BDD/insertCorentinPHP.sql
+++ BDD/insertCorentinPHP.sql
@@ -18,13 +18,17 @@ SELECT idGroupe from groupe WHERE numGroupe = '%s';
 INSERT INTO cours(duree,fk_idModule,fk_idSalle,dates)
 VALUES('%s',%d,%d,'%s');
 
-SELECT idcours 
-FROM cours
-ORDER BY idcours DESC
-LIMIT 1;
+SELECT idcours FROM cours ORDER BY idcours DESC LIMIT 1;
+SELECT idPers FROM personne ORDER BY idPers DESC LIMIT 1;
 
 INSERT INTO personne(nom,prenom,password,mail,sexe,identifiant)
 VALUES ('%s','%s','%s','%s','%s','%s');
 
 INSERT INTO professeur(idProf,burean,telephone,initiales)
 VALUES(%d,'%s','%s','%s');
+
+INSERT INTO chargede(fk_idProf,fk_idModule) VALUES(%d,%d);
+
+INSERT INTO enseigne(fk_idProf,fk_idCours) VALUES (%d,%d);
+
+INSERT INTO suit(fk_idCours,fk_idGroupe) VALUES (%d,%d);
